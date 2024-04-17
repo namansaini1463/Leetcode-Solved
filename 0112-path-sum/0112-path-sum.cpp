2232cead7dec864ca97fdc70162b37190959dc9a
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-04-17 16:52:40 +0530

    Time: 4 ms (84.16%), Space: 19.7 MB (87.33%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -13,6 +13,10 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false;
+        
+        // Check if the current node is a leaf node
+        // If the current node is a leaf and the targetSum equals the value of the leaf node,
+        // it means we have found a path from root to leaf node with the given targetSum
         if(!root->left and !root->right) return targetSum == root->val;
         
         return hasPathSum(root->left, targetSum - root->val) or hasPathSum(root->right, targetSum - root->val);
