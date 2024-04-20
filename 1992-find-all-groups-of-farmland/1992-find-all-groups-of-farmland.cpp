2e006be93c496fbd31df201467983981cbf837b8
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-04-20 23:46:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        vector<vector<int>> result;
+        vector<int> landCoordinates;
+        int r1 = 0, r2 = 0, c1 = 0, c2 = 0;
+        
         int n = land.size();
         int m = land[0].size();
-        vector<vector<int>> result;
+
         vector<vector<bool>> visited(n, vector<bool>(m, false));
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (land[i][j] and !visited[i][j]) {
-                    // New rectangle if land is found and is not visited
-                    int r1 = i, c1 = j;
-                    int r2 = i, c2 = j;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(land[i][j] and !visited[i][j]){
+                    landCoordinates.push_back(i);
+                    landCoordinates.push_back(j);
 
-                    // Expand downwards to find the bottom row of the block
-                    while (r2 + 1 < n and land[r2 + 1][j] and !visited[r2 + 1][j]) {
+                    r2 = i, c2 = j;
+                    while(r2 < n and land[r2][c2] and !visited[r2][c2]){
                         r2++;
-                    }
+                        visited[r2][c2] = true;
+                    } r2--;
+                    while(c2 < m and land[r2][c2]){
+                        c2++;
+                        
+                    }c2--;
 
-                    // Expand rightwards to find the right column of the block
-                    bool valid = true;
-                    while (c2 + 1 < m and valid) {
-                        // Validating each column individually. Agar koi bhi cell 0 hai ussmei ya koi bhi cell visited hai to valid ko false krdia and break krlia tbhi hi. Aur jo last match hua usse consider krlia.
-                        for (int r = r1; r <= r2; r++) {
-                            if (!land[r][c2 + 1]) {
-                                valid = false;
-                                break;
-                            }
-                        }
-                        if (valid) c2++;
-                    }
+                    landCoordinates.push_back(r2);
+                    landCoordinates.push_back(c2);
 
-                    // Mark all cells in the block as visited
-                    for (int r = r1; r <= r2; r++) {
-                        for (int c = c1; c <= c2; c++) {
-                            visited[r][c] = true;
-                        }
-                    }
-
-                    // Add the coordinates of this block to the result list
-                    result.push_back({r1, c1, r2, c2});
+                    result.push_back(landCoordinates);
+                    landCoordinates.clear();
                 }
+                
             }
         }
 
