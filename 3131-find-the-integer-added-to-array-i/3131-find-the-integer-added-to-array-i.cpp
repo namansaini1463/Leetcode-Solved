bfbc5e20424d8969ed1dce6db0f304c64b80dd98
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-04-28 17:13:53 +0530

    Time: 12 ms (25%), Space: 34.1 MB (100%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        /*
+            Since the elements in both the arrays differ by the same number, we can sort the array and
+            then the difference between the ith element of nums1 and nums2 will always be the same
+            - OR
+                We can find the largest(or smallest) numers in the array and return their difference
+        */
+
         int maxi1 = INT_MIN, maxi2 = INT_MIN;
         for(int i = 0; i < nums1.size(); i++){
             maxi1 = max(maxi1, nums1[i]);
