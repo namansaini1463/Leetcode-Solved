ef589695b257e627f794e76bce203d6fa59c05c7
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-05-13 10:16:18 +0530

    Time: 15 ms (8.13%), Space: 10.5 MB (9.21%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -6,7 +6,6 @@ public:
         
         // Checking the first column and changing all its bits to 1 by flipping the entire row
         for(int row = 0; row < rows; row++){
-            cout << grid[row][0] << endl;
             if(grid[row][0] == 0){
                 for(int col = 0; col < cols; col++){
                     grid[row][col] = !grid[row][col];
@@ -18,23 +17,37 @@ public:
             for(auto j : i) cout << j <<" ";
             cout << endl;
         }
+        cout << endl;
         
         // Checking the remaining columns and if the number of zeros in that column are more than the number of ones, then flip the entire column
         for(int col = 1; col < cols; col++){
-            int rowZeroCount = 0;
+            int colZeroCount = 0;
             for(int row = 0; row < rows; row++){
-                if(!grid[row][col]) rowZeroCount++; 
+                if(!grid[row][col]) colZeroCount++; 
             }
 
-            if(rowZeroCount > cols/2){
+            if(colZeroCount * 2 > rows){
                 for(int row = 0; row < rows; row++){
                     grid[row][col] = !grid[row][col];
                 }
             }
         }
 
-        
+        int maxScore = 0;
+        // Finding the result by converting each row of the matrix into equivalent decimal numers
+        for(int row = 0; row < rows; row++){
+            int number = 0;
+            for(int col = 0; col < cols; col++){
+                number = number * 2 + grid[row][col];
+            }
+            maxScore += number;
+        }
 
-        return -1;
+        for(auto i : grid){
+            for(auto j : i) cout << j <<" ";
+            cout << endl;
+        }
+
+        return maxScore;
     }
 };
