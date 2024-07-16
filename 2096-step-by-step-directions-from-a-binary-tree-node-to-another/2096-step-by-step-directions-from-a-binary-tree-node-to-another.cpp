820080a220f71ec73dc05dca21bb73510ab8a4e2
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-07-17 01:13:41 +0530

    Time: 135 ms (96.34%), Space: 118.9 MB (51.76%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -10,8 +10,56 @@
  * };
  */
 class Solution {
+private:
+    TreeNode * findLCA(TreeNode * root, int src, int dst){
+        if(!root) return nullptr;
+
+        if(root->val == src or root->val == dst) return root;
+
+        TreeNode *left = findLCA(root->left, src, dst);
+        TreeNode *right = findLCA(root->right, src, dst);
+
+        if(left and right) return root;
+ 
+        return left ? left  : right;
+    }
+
+    bool findPath(TreeNode * LCA, int targetValue, string &path){
+        if(!LCA) return false;
+
+        if(LCA->val == targetValue) return true;
+
+        path.push_back('L');
+        if(findPath(LCA->left, targetValue, path)){
+            return true;
+        }
+        path.pop_back();
+
+        path.push_back('R');
+        if(findPath(LCA->right, targetValue, path)){
+            return true;
+        }
+        path.pop_back();
+
+        return false;
+    }
 public:
     string getDirections(TreeNode* root, int startValue, int destValue) {
-        
+        TreeNode * LCA = findLCA(root, startValue, destValue);
+
+        string LCAToStartValue = "";
+        string LCAToDestValue = "";
+
+        findPath(LCA, startValue, LCAToStartValue);
+        findPath(LCA, destValue, LCAToDestValue);
+
+        int len = LCAToStartValue.size();
+        string startValueToLCA ="";
+        for(int i = 0; i < len; i++){
+            startValueToLCA += 'U';
+        }
+
+        return startValueToLCA + LCAToDestValue;
+
     }
 };
