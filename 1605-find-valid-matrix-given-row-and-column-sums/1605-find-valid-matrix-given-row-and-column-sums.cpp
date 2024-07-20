ecf3722763da2d413c40c46a4d1c135e9459e699
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-07-20 12:58:21 +0530

    Time: 43 ms (23.25%), Space: 36.2 MB (11.51%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -15,11 +15,11 @@ public:
             }
         }
 
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-               cout << matrix[i][j] << " ";
-            } cout << endl;
-        }
+        // for (int i = 0; i < rows; ++i) {
+        //     for (int j = 0; j < cols; ++j) {
+        //        cout << matrix[i][j] << " ";
+        //     } cout << endl;
+        // }
 
         return matrix;
     }
