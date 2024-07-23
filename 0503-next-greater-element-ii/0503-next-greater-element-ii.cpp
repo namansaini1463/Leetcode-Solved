ebdc128a1f317f6c5a0555d428e91dce483a5fc4
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-07-23 19:04:08 +0530

    Time: 28 ms (24.46%), Space: 26.9 MB (24.55%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,6 +1,41 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+        /*
+            TC: O(n)
+            SC: O(n)
+        */
+
+        int n = nums.size();
+        vector<int> result(n); 
+        stack<int> st;
+
+        // Iterating through the array twice to simulate the circular behavior
+        for (int i = 2 * n - 1; i >= 0; --i) {
+            while (!st.empty() and nums[i % n] >= st.top()) {
+                st.pop();
+            }
+
+            if (i < n) {
+                if (!st.empty()) {
+                    result[i] = st.top();
+                } else {
+                    result[i] = -1;
+                }
+            }
+
+            st.push(nums[i % n]);
+        }
+
+        return result;
+    
+
+        
+        /*
+
+        TC : O(n^2)
+        SC : O(1)
+
         int n = nums.size();
         vector<int> result(n);
 
@@ -18,5 +53,6 @@ public:
         }
 
         return result;
+        */
     }
 };
