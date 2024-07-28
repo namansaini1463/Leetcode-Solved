728a82d0e3c313e546bcdad09d858e37f5c81650
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-07-29 00:55:24 +0530

    Time: 503 ms (51.37%), Space: 199.3 MB (56.82%) - LeetHub

---
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
@@ -0,0 +1,50 @@
+typedef pair<int, int> pii;
+class Solution {
+public:
+    int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
+         unordered_map<int, vector<int>> graph;
+        
+        for(const auto &edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        vector<int> minDistance(n+1, INT_MAX);
+        vector<int> secondMinDistance(n+1, INT_MAX);
+
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+        pq.push({0, 1});
+
+        while(!pq.empty()){
+            auto [timePassed, currentNode] = pq.top();
+            pq.pop();
+
+            if(currentNode == n and secondMinDistance[currentNode] != INT_MAX)
+                return secondMinDistance[n];
+
+            int currentSlot = timePassed/change;
+            if(currentSlot % 2 == 1){ //currently in the red signal
+                timePassed = change * (currentSlot + 1); // wait until the next region
+            }
+
+            for(const auto &adjNode : graph[currentNode]){
+                if(minDistance[adjNode] > timePassed + time){
+                    secondMinDistance[adjNode] = minDistance[adjNode];
+                    minDistance[adjNode] =  timePassed + time;
+                    pq.push({timePassed + time, adjNode});
+                } else if(secondMinDistance[adjNode] > timePassed + time and 
+                        minDistance[adjNode] != timePassed + time){
+                      secondMinDistance[adjNode] = timePassed + time;      
+                    pq.push({timePassed + time, adjNode});
+
+                }
+            }
+        }
+
+        return -1;
+
+    }
+};
