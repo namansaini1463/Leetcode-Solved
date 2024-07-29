6b410f1599698f861c328f77372b4d59b5ca7328
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-07-29 18:06:23 +0530

    Time: 23 ms (77.81%), Space: 15.3 MB (89.92%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        // Making the graph in the form of an adjacency matrix
+        vector<vector<int>> graph(n, vector<int>(n, INT_MAX));
+        
+        for(int i = 0; i < n; i++){
+            graph[i][i] = 0;
+        }
+
+        for(const auto &edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+
+            graph[u][v] = w;
+            graph[v][u] = w;
+        }
+
+        for(int via = 0; via < n; via++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(graph[i][via] != INT_MAX and graph[via][j] != INT_MAX){
+                        graph[i][j] = min(graph[i][j], graph[i][via] + graph[via][j]);
+                    }
+                } 
+            }
+        }
+
+        int minNeighboursCount = INT_MAX;
+        int cityWithSmallestNumberOfNeighbors = n;
+        for(int i = 0; i < n; i++){
+            int neighbourCount = 0;
+            for(int j = 0; j < n; j++){
+                if(graph[i][j] <= distanceThreshold) neighbourCount++;
+            } 
+
+            if(neighbourCount <= minNeighboursCount){
+                minNeighboursCount = neighbourCount;
+                cityWithSmallestNumberOfNeighbors = i;
+            }
+        }
+        
+
+        return cityWithSmallestNumberOfNeighbors;
+
+    }
+}; 
