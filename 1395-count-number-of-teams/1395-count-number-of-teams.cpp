93582148715fc176d4eb6574bc694ce9da2a35d5
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-07-29 09:31:27 +0530

    Time: 2898 ms (5.06%), Space: 11.7 MB (40.04%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int n = rating.size();
+        int teamsCount = 0;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                for(int k = j+1; k < n; k++){
+                    if(rating[i] < rating[j] and rating[j] < rating[k]) teamsCount++;
+                    if(rating[i] > rating[j] and rating[j] > rating[k]) teamsCount++;
+
+                }
+            }
+        }
+
+        return teamsCount;
+    }
+};
