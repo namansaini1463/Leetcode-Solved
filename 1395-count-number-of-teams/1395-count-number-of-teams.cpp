0fedf0d960a8d61145a923d6b3d4b12ca19874df
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-07-29 12:23:51 +0530

    Time: 24 ms (88.09%), Space: 11.7 MB (51.74%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
+        /*
         int n = rating.size();
         int teamsCount = 0;
         
@@ -12,6 +13,37 @@ public:
 
                 }
             }
+        }
+
+        return teamsCount;
+        */
+
+        int n = rating.size();
+        int teamsCount = 0;
+
+        for(int j = 1; j < n-1; j++){
+            int smallerLeft = 0;
+            int largerRight = 0;
+            int smallerRight = 0;
+            int largerLeft = 0;
+
+            for(int i = 0; i < j; i++){
+                if(rating[i] < rating[j]){
+                    smallerLeft++;
+                } else {
+                    largerLeft++;
+                }
+            }
+
+            for(int k = j+1; k < n; k++){
+                if(rating[j] < rating[k]){
+                    largerRight++;
+                } else {
+                    smallerRight++;
+                }
+            } 
+
+            teamsCount += smallerLeft*largerRight + largerLeft*smallerRight;
         }
 
         return teamsCount;
