b1032b5c250f2436c1f0b0cf4011f7400bdb8f91
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-08-02 12:09:00 +0530

    Time: 72 ms (40.47%), Space: 83.1 MB (57.56%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -3,20 +3,23 @@ public:
     int minSwaps(vector<int>& nums) {
         int n = nums.size();
         int totalOnes = 0;
-        for(const auto &num : nums){
-            if(num) totalOnes++;
+        for (const auto& num : nums) {
+            if (num)
+                totalOnes++;
         }
 
-        if (totalOnes == 0 or totalOnes == n) return 0; // No swaps needed if there are no 1s or all are 1s
+        if (totalOnes == 0 or totalOnes == n)
+            return 0; // No swaps needed if there are no 1s or all are 1s
 
         int currentOnes = 0, maxOnes = 0;
 
         int i = 0, j = 0;
-        while( j < 2 * n){
-            if(nums[j%n] == 1) currentOnes++;
+        while (j < 2 * n) {
+            if (nums[j % n] == 1)
+                currentOnes++;
 
-            if(j - i + 1 > totalOnes){
-                currentOnes -= nums[i%n];
+            if (j - i + 1 > totalOnes) {
+                currentOnes -= nums[i % n];
                 i++;
             }
 
@@ -26,5 +29,4 @@ public:
 
         return totalOnes - maxOnes;
     }
-
 };
