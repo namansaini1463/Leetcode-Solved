76eaa007d02e5cd90d821ac6f39ee8acdccf2cab
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2024-08-04 18:17:18 +0530

    Time: 1028 ms (5.17%), Space: 27.2 MB (31.24%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
+        priority_queue<int, vector<int>, greater<int>> subArraySums;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int subArraySum = 0;
+                for(int idx = i; idx <= j; idx++){
+                    subArraySum += nums[idx];
+                }
+                subArraySums.push(subArraySum);
+            }
+        }
+
+        int l = 1;
+        while(l++ != left){
+            subArraySums.pop();
+        }
+
+        int r = l-1;
+
+        long long sum = 0;
+        while(r <= right){
+            sum += subArraySums.top();
+            subArraySums.pop();
+            r++;
+        }
+        return static_cast<int>(sum % ((int)(1e9+7)));
+    }
+
+    /*
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
         //finding all the subarray sums and storing them in an array
         vector<int> subArraySums;
         for(int i = 0; i < n; i++){
@@ -22,4 +51,5 @@ public:
 
         return static_cast<int>(sum % ((int)(1e9+7)));
     }
+    */
 };
