4496949dd34f4a02ba2df2183387efa57575e2b6
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2025-01-01 10:18:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (38.26%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -3,16 +3,24 @@ public:
     int maxScore(string s) {
         int onesCount = 0;
         int zerosCount = 0;
+
+        // Precomputing the number of ones in the string
         for(int i = 0; i < s.size(); i++){
             if(s[i] == '1') onesCount++;
         }
 
+        // Variable to store the maximum score of the string
         int maxScore = INT_MIN;
 
+        // i marks as the splitter for the two substrings
+        // s.size() - 1 becoz, the strings are non empty
         for(int i = 0; i < s.size() - 1; i++){
+            // current score = zerosCount + onesCount
             if(s[i] == '0'){
+                // increase the zero count and calculate current score then update maxScore
                 maxScore = max(maxScore, ++zerosCount + onesCount);
             } else {
+                // decrease the one count and calculate current score then update maxScore
                 maxScore = max(maxScore, zerosCount + --onesCount);
             }
         }
