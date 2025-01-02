e87cd8474e9fc047ae25e574a2aa42ce5d6d36bc
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2025-01-02 12:33:46 +0530

    Time: 3 ms (89.32%), Space: 69.8 MB (63.35%) - LeetHub

---
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
@@ -0,0 +1,47 @@
+class Solution {
+private:
+    bool isVowel(char ch) {
+        static const unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+        return vowels.count(ch) > 0;
+    }
+
+    bool checkString(const string& s) {
+        return isVowel(s.front()) && isVowel(s.back());
+    }
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        vector<int> result;
+
+        vector<int> prefixSum(words.size(), 0);
+
+        // Calculating prefix sum for words that start and end with vowels
+        for (int i = 0; i < words.size(); i++) {
+            if (i == 0) {
+                // For the first element, directly assign because there is no `i-1` index
+                prefixSum[i] = checkString(words[i]) ? 1 : 0;
+            } else {
+                // Add the current value to the previous prefix sum
+                if (checkString(words[i])) {
+                    prefixSum[i] = prefixSum[i - 1] + 1;
+                } else {
+                    prefixSum[i] = prefixSum[i - 1];
+                }
+            }
+        }
+
+        // Process each query
+        for (const auto &query : queries) {
+            int left = query[0];
+            int right = query[1];
+
+            // Use prefix sum to calculate the result for the range
+            if (left == 0) {
+                result.push_back(prefixSum[right]);
+            } else {
+                result.push_back(prefixSum[right] - prefixSum[left - 1]);
+            }
+        }
+
+        return result;
+    }
+};
