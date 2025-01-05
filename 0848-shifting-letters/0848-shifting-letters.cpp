175b7c58f09155e11a7c33b4687fbaec3441585a
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2025-01-05 12:31:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0848-shifting-letters/0848-shifting-letters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n = shifts.size();
+        vector<int> prefixReverseSum(n, 0);
+        prefixReverseSum[n-1] = shifts[n-1];
+        for(int i = shifts.size() - 2; i >= 0; --i){
+            prefixReverseSum[i] = prefixReverseSum[i+1] + shifts[i];
+        }
+        
+        for(const auto &i : prefixReverseSum) cout << i << " ";
+        cout << endl;
+
+        for(int i = 0; i < n; i++){
+            s[i] = 'a' + (s[i] - 'a' + prefixReverseSum[i]) % 26;
+        }
+
+        return s;
+    }
+};
