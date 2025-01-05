688677cd33f3746572e07e8e186b12ecb88f57dd
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2025-01-05 23:48:08 +0530

    Time: 0 ms (100%), Space: 97.8 MB (69.79%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int n = s.size();
 
@@ -21,4 +22,33 @@ public:
         return s;
 
     }
+*/
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n = s.size();
+        vector<int> movements(n + 1, 0);  // Difference array (size n + 1)
+
+        // Process shifts using the difference array
+        for (const auto &shift : shifts) {
+            int startIndex = shift[0];
+            int endIndex = shift[1];
+            int direction = shift[2];
+
+            movements[startIndex] += direction ? 1 : -1;
+            movements[endIndex + 1] += direction ? -1 : 1;
+        }
+
+        // Compute the prefix sum for actual movements
+        for (int i = 1; i < n; i++) {
+            movements[i] += movements[i - 1];
+        }
+
+        // Apply shifts to the string
+        for (int i = 0; i < n; i++) {
+            int shiftValue = (movements[i] % 26 + 26) % 26; // Ensure non-negative
+            s[i] = 'a' + (s[i] - 'a' + shiftValue) % 26;
+        }
+
+        return s;
+    }
+
 };
