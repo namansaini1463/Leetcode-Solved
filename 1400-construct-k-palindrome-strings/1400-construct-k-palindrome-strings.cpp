3782f2b99713d681d10c959ef17e50bdf3f8c13d
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2025-01-11 13:07:33 +0530

    Time: 4 ms (62.59%), Space: 15 MB (51.75%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     bool canConstruct(string s, int k) {
+        // Optimising the code a bit
         if(s.size() < k) return false;
         if(s.size() == k) return true;
 
+        // Creating a frequency array that only stores/tracks the odd chars
         int count[26] = {0};
 
         for(int i = 0; i < s.size(); i++){
             count[s[i] - 'a'] ^= 1;
         }
 
+        // Count the number of odd chars,
+        // If they are less than or equal to 'k', then we can partition the string, otherwise not
         int oddChars = 0;
         for(const auto &i : count){
             oddChars += i;
