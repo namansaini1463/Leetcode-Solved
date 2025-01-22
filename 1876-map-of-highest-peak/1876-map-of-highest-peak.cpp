39099202e3e6c4f32a6600efaf36d087ff6e0a4b
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2025-01-22 12:14:45 +0530

    Time: 85 ms (47.94%), Space: 113.5 MB (49.03%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -45,17 +45,15 @@ public:
             distance++;
         }
 
+        /*
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                cout << visited[i][j] << " ";
             }
             cout << endl;
         }
-
-        
-
+        */
+   
         return result;
-
-
     }
 };
