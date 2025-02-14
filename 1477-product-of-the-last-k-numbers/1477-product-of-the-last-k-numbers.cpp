09c4d2f15b851185481867db782f35ff48ec4eba
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2025-02-14 15:26:49 +0530

    Time: 24 ms (51.45%), Space: 78 MB (77.84%) - LeetHub

---
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+
+class ProductOfNumbers {
+public:
+    vector<int> prefixProducts;
+
+    ProductOfNumbers() {
+        prefixProducts.push_back(1);  
+    }
+    
+    void add(int num) {
+        if (num == 0) {
+            prefixProducts.clear();
+            prefixProducts.push_back(1);  // Reset the prefix product sequence on 0.
+        } else {
+            prefixProducts.push_back(prefixProducts.back() * num);
+        }
+    }
+    
+    int getProduct(int k) {
+        int n = prefixProducts.size();
+        if (k >= n) return 0;  
+        return prefixProducts[n - 1] / prefixProducts[n - k - 1];
+    }
+};
