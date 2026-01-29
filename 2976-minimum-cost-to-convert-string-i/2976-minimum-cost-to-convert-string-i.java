7d3ec0f92822eccd56b203b12011c4de84830ee9
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2026-01-29 22:51:44 +0530

    Time: 22 ms (66.67%), Space: 48.2 MB (16.49%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
@@ -1,3 +1,97 @@
+/*
+class Solution {
+    private void addMinimumEdge(List<List<int[]>> adj, int u, int v, int w) {
+        for (int[] edge : adj.get(u)) {
+            int from = edge[0];
+            int weight = edge[1];
+
+            if (from == v) {
+                edge[1] = Math.min(weight, w);
+                return;
+            }
+        }
+
+        adj.get(u).add(new int[] { v, w });
+    }
+
+    private int getMinCost(List<List<int[]>> adj, int u, int v) {
+        int[] minCost = new int[26];
+        Arrays.fill(minCost, Integer.MAX_VALUE);
+        minCost[u] = 0;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        pq.add(new int[] { 0, u });
+
+        while (!pq.isEmpty()) {
+            int[] pair = pq.poll();
+            int distance = pair[0];
+            int node = pair[1];
+            
+            if(distance > minCost[node]) continue;
+            
+            if (node == v) return distance;
+
+            for (int[] edge : adj.get(node)) {
+                int nei = edge[0];
+                int w = edge[1];
+
+
+                if (distance + edge[1] < minCost[nei]) {
+                    minCost[nei] = distance + edge[1];
+                    pq.add(new int[] { distance + edge[1], nei });
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        int n = original.length;
+
+        int sourceLength = source.length();
+
+        List<List<int[]>> adjList = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
+            int w = cost[i];
+
+            addMinimumEdge(adjList, u, v, w);
+        }
+
+        long minCost = 0;
+        for (int i = 0; i < sourceLength; i++) {
+            int u = source.charAt(i) - 'a';
+            int v = target.charAt(i) - 'a';
+
+            int currentMinCostForU_V = getMinCost(adjList, u, v);
+
+            if (currentMinCostForU_V == -1)
+                return -1;
+            minCost += currentMinCostForU_V;
+        }
+
+        // for (int i = 0; i < 26; i++) {
+        //     System.out.print(i + ": ");
+        //     for (int[] edge : adjList.get(i)) {
+        //         System.out.println(edge[0] + " " + edge[1]);
+        //     }
+        //     System.out.println();
+        // }
+
+        return minCost;
+
+    }
+}
+*/
+
+
+
 class Solution {
     private void addMinimumEdge(List<List<int[]>> adj, int u, int v, int w){
         for(int[] edge : adj.get(u)){
@@ -68,3 +162,4 @@ class Solution {
         return minCost;
     }
 }
+
