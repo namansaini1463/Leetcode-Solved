c7d8a9c5475fe23edc2cb551c21f992a89ea4fa7
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2026-02-04 23:29:22 +0530

    Time: 93 ms (50.51%), Space: 100.4 MB (71.13%) - LeetHub

---
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int secondMinimum(int n, int[][] edges, int time, int change) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n + 1; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
+        }
+
+        int[] minimumDistance = new int[n + 1];
+        Arrays.fill(minimumDistance, Integer.MAX_VALUE);
+        minimumDistance[1] = 0;
+
+        int[] secondMinimumDistance = new int[n + 1];
+        Arrays.fill(secondMinimumDistance, Integer.MAX_VALUE);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        pq.offer(new int[] { 0, 1 }); // {distance, node}
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+
+            int d = (curr[0] / change) % 2 == 0 ? curr[0] : ((curr[0] / change) + 1) * change;
+            int node = curr[1];
+
+            if (node == n && secondMinimumDistance[node] != Integer.MAX_VALUE)
+                return secondMinimumDistance[node];
+
+            for (int adj : adjList.get(node)) {
+                if (d + time < minimumDistance[adj]) {
+                    secondMinimumDistance[adj] = minimumDistance[adj];
+                    minimumDistance[adj] = d + time;
+                    pq.add(new int[] { d + time, adj });
+                } else if (d + time < secondMinimumDistance[adj] && d + time > minimumDistance[adj]) {
+                    secondMinimumDistance[adj] = d + time;
+                    pq.add(new int[] { d + time, adj });
+                }
+            }
+        }
+
+        return -1;
+    }
+}
