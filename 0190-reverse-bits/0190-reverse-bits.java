797aeae6abd26ee0f7f164aa5d2944c39b5d3f42
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2026-02-16 07:34:26 +0530

    Time: 0 ms (100%), Space: 42.5 MB (71.14%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -2,10 +2,10 @@ class Solution {
     public int reverseBits(int n) {
         int result = 0;
         for(int i = 0; i < 32; i++){
-            int bit = n&1;
-            n = n >> 1;
+            int bit = n&1; // Get last bit
+            n = n >> 1;  // Move the number right by 1 bit
 
-            result = result | bit << (31 - i);
+            result = result | bit << (31 - i); 
         }
 
         return result;
