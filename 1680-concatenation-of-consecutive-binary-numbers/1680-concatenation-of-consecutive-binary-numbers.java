173884ee5588383bf57bf086018b8484f9405ac3
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2026-02-28 10:05:11 +0530

    Time: 24 ms (98.84%), Space: 41.8 MB (100%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,18 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+
+    public int concatenatedBinary(int n) {
+        long result = 0;
+        int length = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                length++;
+            }
+
+            result = ((result << length) + i) % MOD;
+        }
+
+        return (int) result;
+    }
+}
