91477205d975f8ad53cc798ba7486eb0addf609d
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2026-03-28 05:22:47 +0530

    Time: 1 ms (99.47%), Space: 46.9 MB (63.2%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -1,93 +1,18 @@
 class Solution {
     public boolean areSimilar(int[][] mat, int k) {
-
-        int[][] originalMat = new int[mat.length][];
-        for (int i = 0; i < mat.length; i++) {
-            originalMat[i] = Arrays.copyOf(mat[i], mat[i].length);
-        }
-
-        int n = mat.length;
-        int m = mat[0].length;
-
+        int n = mat.length, m = mat[0].length;
         k = k % m;
-        
-        // Process the matrix
-        for(int i = 0; i < n; i++){
-            if(i % 2 == 0){
-                int leftPointer = 0, rightPointer =  k - 1;
 
-                // Reverse 0..k-1
-                while(leftPointer < rightPointer){
-                    int temp = mat[i][leftPointer];
-                    mat[i][leftPointer] = mat[i][rightPointer];
-                    mat[i][rightPointer] = temp;
-
-                    leftPointer++; rightPointer--;
-                }
-
-                // Reverse k...m-1
-                leftPointer = k; rightPointer = m-1;
-                while(leftPointer < rightPointer){
-                    int temp = mat[i][leftPointer];
-                    mat[i][leftPointer] = mat[i][rightPointer];
-                    mat[i][rightPointer] = temp;
-
-                    leftPointer++; rightPointer--;
-                }
-
-                // Reverse 0...m-1
-                leftPointer = 0; rightPointer = m-1;
-                while(leftPointer < rightPointer){
-                    int temp = mat[i][leftPointer];
-                    mat[i][leftPointer] = mat[i][rightPointer];
-                    mat[i][rightPointer] = temp;
-
-                    leftPointer++; rightPointer--;
-                }
-            } else {
-                int leftPointer = 0, rightPointer =  m - k - 1;
-
-                // Reverse 0..k
-                while(leftPointer < rightPointer){
-                    int temp = mat[i][leftPointer];
-                    mat[i][leftPointer] = mat[i][rightPointer];
-                    mat[i][rightPointer] = temp;
-
-                    leftPointer++; rightPointer--;
-                }
-
-                // Reverse k+1...m-1
-                leftPointer = m - k; rightPointer = m-1;
-                while(leftPointer < rightPointer){
-                    int temp = mat[i][leftPointer];
-                    mat[i][leftPointer] = mat[i][rightPointer];
-                    mat[i][rightPointer] = temp;
-
-                    leftPointer++; rightPointer--;
-                }
-
-                // Reverse 0...m-1
-                leftPointer = 0; rightPointer = m-1;
-                while(leftPointer < rightPointer){
-                    int temp = mat[i][leftPointer];
-                    mat[i][leftPointer] = mat[i][rightPointer];
-                    mat[i][rightPointer] = temp;
-
-                    leftPointer++; rightPointer--;
-                }
-            }
-        }
-
-        // Compare the matrix
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(originalMat[i][j] != mat[i][j]) return false;
+                int shiftedIndex = (i % 2 == 0)
+                    ? (j + k) % m        // left shift
+                    : (j - k + m) % m;   // right shift
+
+                if (mat[i][j] != mat[i][shiftedIndex]) return false;
             }
         }
 
-        // System.out.println(Arrays.deepToString(mat));
-
         return true;
-
     }
 }
