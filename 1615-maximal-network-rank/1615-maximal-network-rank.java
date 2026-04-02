150d5dd9bc43f050ef6854ff05fef3ed88908fbe
Naman Saini <69137810+namansaini1463@users.noreply.github.com>
2026-04-03 04:47:27 +0530

    Time: 19 ms (48.12%), Space: 47.7 MB (53.19%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maximalNetworkRank(int n, int[][] roads) {
+        List<Set<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new HashSet<>());
+        }
+
+        int[] indegree = new int[n];
+
+        for(int[] road : roads){
+            int u = road[0], v = road[1];
+
+            indegree[u]++; indegree[v]++;
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        int maxNetworkRank = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i != j){
+                    maxNetworkRank = Math.max(maxNetworkRank, indegree[i] + indegree[j] - (adj.get(i).contains(j) ? 1 : 0));
+                }
+            }
+        }
+
+        return maxNetworkRank;
+        
+        
+    }
+}
