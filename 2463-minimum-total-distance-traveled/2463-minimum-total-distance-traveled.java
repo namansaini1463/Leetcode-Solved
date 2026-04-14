3f74fb788ad684512c3ea5956349c7fe8a2af736
Naman Saini <namansaini1463@gmail.com>
2026-04-14 23:45:03 +0530

    Time: 57 ms (38.71%), Space: 72.6 MB (37.1%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
@@ -1,36 +1,11 @@
 class Solution {
-    List<Integer> robots;
-    List<Integer> factories;
-    long[][] dp;
-
-    private long findMinimumDistance(int i, int j) {
-
-        if (i >= this.robots.size())
-            return 0;
-        if (j >= this.factories.size())
-            return Long.MAX_VALUE;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        long takeNext = findMinimumDistance(i + 1, j + 1);
-
-        long takeCurrentFactory = Long.MAX_VALUE;
-
-        if (takeNext != Long.MAX_VALUE) {
-            takeCurrentFactory = Math.abs(this.robots.get(i) - this.factories.get(j)) + takeNext;
-        }
-        long skipCurrentFactory = 0 + findMinimumDistance(i, j + 1);
-
-        return dp[i][j] = Math.min(takeCurrentFactory, skipCurrentFactory);
-    }
-
     public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+
         Collections.sort(robot);
         Arrays.sort(factory, Comparator.comparingInt(a -> a[0]));
 
-        // Expand the factory array to a 1d array 
         List<Integer> factories = new ArrayList<>();
+
         for (int[] f : factory) {
             int count = f[1];
             while (count-- > 0) {
@@ -38,24 +13,30 @@ class Solution {
             }
         }
 
-        this.robots = robot;
-        this.factories = factories;
+        int r = robot.size();
+        int f = factories.size();
 
-        this.dp = new long[robots.size()][factories.size()];
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[0].length; j++) {
-                dp[i][j] = -1;
+        long[][] dp = new long[r + 1][f + 1];
+
+        // base case
+        for (int i = 0; i < r; i++) {
+            dp[i][f] = Long.MAX_VALUE;
+        }
+
+        for (int i = r - 1; i >= 0; i--) {
+            for (int j = f - 1; j >= 0; j--) {
+
+                long take =  Long.MAX_VALUE;
+                if(dp[i + 1][j+1] < Long.MAX_VALUE){
+                    take = Math.abs(robot.get(i) - factories.get(j)) + dp[i + 1][j + 1];
+                }
+
+                long skip = dp[i][j + 1];
+
+                dp[i][j] = Math.min(take, skip);
             }
         }
 
-        int i = 0, j = 0;
-
-        long minimumDistance = findMinimumDistance(i, j);
-
-        // System.out.println(robot);
-        // System.out.println(factories);
-        // System.out.println(Arrays.deepToString(factory));
-
-        return minimumDistance;
+        return dp[0][0];
     }
 }
