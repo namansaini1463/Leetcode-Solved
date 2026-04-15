12b3482d9cb1b0020275de837e2ad251a9f1dab2
Naman Saini <namansaini1463@gmail.com>
2026-04-16 00:15:33 +0530

    Time: 0 ms (100%), Space: 46.4 MB (91.56%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,46 +1,14 @@
 class Solution {
     public int closestTarget(String[] words, String target, int startIndex) {
         int n = words.length;
-        
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            adj.add(new ArrayList<>());
-        }
 
         for(int i = 0; i < n; i++){
-            adj.get(i).add((i+1)%n);
-            adj.get(i).add((i-1+n)%n);
+            // forward traversal
+            if(words[(startIndex + i) % n].equals(target)) return i;
+
+            //backward traversal
+            if(words[(startIndex - i + n) % n].equals(target)) return i;
         }
-
-        boolean[] visited = new boolean[n];
-
-        Deque<Integer> q = new ArrayDeque<>();
-        q.offer(startIndex);
-        visited[startIndex] = true;
-
-        int steps = 0;
-
-        System.out.println();
-
-        while(!q.isEmpty()){
-            int size = q.size();
-            while(size-- > 0){
-
-                int front = q.poll();
-
-                if(words[front].equals(target)) return steps;
-
-                for(int adjNode : adj.get(front)){
-                    if(!visited[adjNode]){
-                        q.offer(adjNode);
-                        visited[adjNode] = true;
-                    }
-                }
-            }
-            steps++;
-        }
-
-        // System.out.println(adj);
 
         return -1;
     }
