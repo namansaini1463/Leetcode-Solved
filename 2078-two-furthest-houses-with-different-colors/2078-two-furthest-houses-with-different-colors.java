b3ec65294f6ed49854ea4faedc3465864facab26
Naman Saini <namansaini1463@gmail.com>
2026-04-20 06:40:44 +0530

    Time: 0 ms (100%), Space: 43.5 MB (11.42%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -1,17 +1,24 @@
 class Solution {
     public int maxDistance(int[] colors) {
         int n = colors.length;
+        int max = 0;
 
-        int maximumDistance = 0;
-
-        for(int i = 0; i < n; i++){
-            for(int j = n - 1; j > i; j--){
-                if(colors[i] != colors[j]){
-                    maximumDistance = Math.max(maximumDistance, j - i);
-                }
+        // Fix left = 0, scan from right
+        for (int j = n - 1; j > 0; j--) {
+            if (colors[0] != colors[j]) {
+                max = Math.max(max, j - 0); 
+                break; 
             }
         }
 
-        return maximumDistance;
+        // Fix right = n-1, scan from left
+        for (int i = 0; i < n - 1; i++) {
+            if (colors[i] != colors[n - 1]) {
+                max = Math.max(max, (n - 1) - i);
+                break; // leftmost valid i from right
+            }
+        }
+
+        return max;
     }
 }
