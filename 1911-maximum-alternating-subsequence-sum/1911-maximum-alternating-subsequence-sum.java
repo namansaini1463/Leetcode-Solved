a850519ebc93d203c683f899311e670bfea892f4
Naman Saini <namansaini1463@gmail.com>
2026-04-23 20:26:16 +0530

    Time: 28 ms (67.72%), Space: 104.5 MB (54.9%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -2,6 +2,39 @@ class Solution {
     int[] nums;
     int n;
     long[][] dp;
+
+    public long maxAlternatingSum(int[] nums) {
+        this.nums = nums;
+        this.n = nums.length;
+        this.dp = new long[n+1][2];
+
+        // Base case { if(idx >= n) return dp[idx][isEvenIndex] = 0; }
+        dp[n][0] = 0; dp[n][1] = 0;
+        for(int idx = n-1; idx >= 0; idx--){
+            for(int isEvenIndex = 0; isEvenIndex < 2; isEvenIndex++){
+
+                long pick = 0;
+                if(isEvenIndex == 1){
+                    pick = nums[idx] + dp[idx+1][0];
+                } else {
+                    pick = -1 * nums[idx] + dp[idx+1][1];
+                }
+
+                long skip = 0 + dp[idx+1][isEvenIndex];
+
+                dp[idx][isEvenIndex] = Math.max(pick, skip);
+            }
+        }
+
+        return dp[0][1];
+    }
+}
+
+/*
+class Solution {
+    int[] nums;
+    int n;
+    long[][] dp;
     
     private long f(int idx, int isEvenIndex){
         if(idx >= n) return dp[idx][isEvenIndex] = 0;
@@ -32,3 +65,4 @@ class Solution {
         return f(0, 1); // 1 -> isEvenIndex = true
     }
 }
+*/
