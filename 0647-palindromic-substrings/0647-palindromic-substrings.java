132b29f3d18333e3a4fd7e8cc4663db7198d88ab
Naman Saini <namansaini1463@gmail.com>
2026-05-24 16:53:56 +0530

    Time: 20 ms (26.18%), Space: 46.4 MB (25.67%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,40 +1,29 @@
 class Solution {
-    int[][] palindrome;
-
-    public int isPalindrome(String s, int start, int end) {
-        if (palindrome[start][end] != -1) return palindrome[start][end];
-
-        int origStart = start;
-        int origEnd = end;
-
-        while (start <= end) {
-            if (s.charAt(start++) != s.charAt(end--)) {
-                return palindrome[origStart][origEnd] = 0;
-            }
-        }
-
-        return palindrome[origStart][origEnd] = 1;
-    }
-
     public int countSubstrings(String s) {
         int n = s.length();
-        if (n == 1) return 1;
 
-        palindrome = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(palindrome[i], -1);
-        }
+        boolean[][] palindrome = new boolean[n][n];
 
         int count = 0;
-        // Check ALL possible start and end combinations
-        for (int start = 0; start < n; start++) {
-            for (int end = start; end < n; end++) {
-                if (isPalindrome(s, start, end) == 1) {
-                    count++;
+
+        for(int L = 1; L <= n; L++){
+            for(int i = 0; i + L - 1 < n; i++){
+                int j = i + L - 1;
+
+                if(i == j) {
+                    palindrome[i][i] = true;
+                } else if(j == i + 1){
+                    palindrome[i][j] = s.charAt(i) == s.charAt(j);
+                } else {
+                    palindrome[i][j] = s.charAt(i) == s.charAt(j) &&  palindrome[i+1][j-1];
                 }
+
+                if(palindrome[i][j]) count++;
             }
         }
 
         return count;
+
+
     }
 }
