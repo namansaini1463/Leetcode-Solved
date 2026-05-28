4744b2db79f01360d6e7b19fecabbba89d4fe3d4
Naman Saini <namansaini1463@gmail.com>
2026-05-28 19:24:12 +0530

    Time: 7 ms (97.97%), Space: 65.2 MB (72.88%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,33 +1,55 @@
 class Solution {
-    public List<List<String>> partition(String s) {
-        List<String> partitions = new LinkedList<>();
-        List<List<String>> result = new ArrayList<>();
+    boolean[][] isPalindrome;
 
-        findPartitions(0, s, partitions, result);
-
-        return result;
-    }
-
-    public void findPartitions(int index, String s, List<String> partitions, List<List<String>> result){
-        if(index == s.length()){
-            result.add(new ArrayList<>(partitions));
+    public void solve(String s, int idx, List<String> currentPartition, List<List<String>> allPartitions){
+        int n = s.length();
+        if(idx == n){
+            allPartitions.add(new ArrayList<>(currentPartition));
             return;
         }
 
-        for(int i = index; i < s.length(); i++){
-            if(isPalindrome(s, index, i)){
-                partitions.addLast(s.substring(index, i+1));
-                findPartitions(i+1, s, partitions, result);
-                partitions.removeLast();
+        for(int j = idx; j < n; j++){
+            if(isPalindrome[idx][j]){
+                currentPartition.add(s.substring(idx, j+1));
+                solve(s, j+1, currentPartition, allPartitions);
+                currentPartition.remove(currentPartition.size() - 1);
             }
         }
+
+
     }
 
-    public boolean isPalindrome(String s, int startIndex, int endIndex){
-        while(startIndex <= endIndex){
-            if(s.charAt(startIndex++) != s.charAt(endIndex--)) return false;
+    public List<List<String>> partition(String s) {
+        int n = s.length();
+
+        this.isPalindrome = new boolean[n][n];
+        for(int i = 0; i < n; i++){
+            isPalindrome[i][i] = true;
         }
 
-        return true;
+        for(int L = 2; L <= n; L++){
+            for(int i = 0; i + L - 1 < n; i++){
+                int j = i + L - 1;
+
+                if(s.charAt(i) == s.charAt(j)){
+                    if(L == 2){
+                        isPalindrome[i][j] = true;
+                    } else {
+                        isPalindrome[i][j] = isPalindrome[i+1][j-1];
+                    }
+                }
+            }
+        }
+
+        List<List<String>> allPartitions = new ArrayList<>();
+        List<String> currentPartition = new ArrayList<>();  
+
+        solve(s, 0, currentPartition, allPartitions);
+        
+        // for(int i = 0; i < n; i++){
+        //     System.out.println(Arrays.toString(isPalindrome[i]));
+        // }
+
+        return allPartitions;
     }
 }
